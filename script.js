6fe5070659b91c4b085362d9f677a4099bcd2f84
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-10 18:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function insert_Row() {
 	let col1 = document.createElement("td");
 	col1.innerText = "New Cell1";
 	let col2 = document.createElement("td");
-	col1.innerText = "New Cell1";
+	col2.innerText = "New Cell2";
 	row.append(col1,col2);
 	table.insertBefore(row, chid1);
   
